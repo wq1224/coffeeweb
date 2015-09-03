5dc90d4e4cb2b8d68fe547eb655e80a76ca46ba2
Joice Wang <joice.wang@sap.com>
2015-09-03 13:37:16 +0800

    arrange js

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -19,5 +19,42 @@ $(document).ready(function() {
 		$(this).parent().addClass("active");
 		$(this).parent().siblings().removeClass("active");
 	});
+	$("span.menu").click(function(){
+		$(".top-nav ul").slideToggle("slow" , function(){
+		});
+	});
+
+	var filterList = {
+		init: function () {
+			// MixItUp plugin
+			// http://mixitup.io
+			$('#portfoliolist').mixitup({
+				targetSelector: '.portfolio',
+				filterSelector: '.filter',
+				effects: ['fade'],
+				easing: 'snap',
+				// call the hover effect
+				onMixEnd: filterList.hoverEffect()
+			});				
+		},
+		hoverEffect: function () {
+			// Simple parallax effect
+			$('#portfoliolist .portfolio').hover(
+				function () {
+					$(this).find('.label').stop().animate({bottom: 0}, 200, 'easeOutQuad');
+					$(this).find('img').stop().animate({top: -30}, 500, 'easeOutQuad');				
+				},
+				function () {
+					$(this).find('.label').stop().animate({bottom: -40}, 200, 'easeInQuad');
+					$(this).find('img').stop().animate({top: 0}, 300, 'easeOutQuad');								
+				}		
+			);				
+		}
+ 	};
+	filterList.init();
+
+	$().UItoTop({ easingType: 'easeOutQuart' });
 	 
 });
+
+	
